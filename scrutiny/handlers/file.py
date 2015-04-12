3769e9db380b06ff45b0106dd5c4d727ecb2cb2f
ardinor <jordan@defestri.org>
2015-04-12 17:01:20 +1000

    Split up the existing code, make it easier to read

--- scrutiny/handlers/file.py
+++ scrutiny/handlers/file.py
@@ -1,5 +1,12 @@
 from datetime import timedelta, datetime
 
+"""
+Instead of parsing log files, perhaps it would be better to basically tail them,
+subscribe to changes and when we see an entry that matches our regex, add it.
+Or, scan through the log files on startup, then set the datetime of the last entry
+we see as some 'last checked' value, then tail the logs.
+"""
+
 
 class LogFileReader():
 
@@ -48,6 +55,76 @@ class LogFileReader():
 
 
         return breakin_attempt, banned_ip
+
+
+    def get_file_content(self, log_file):
+
+        if os.path.splitext(log_file)[1] == '.gz':
+            # Use zcat?
+            with open(gzip.open(os.path.join(log_dir, log_file), 'r')) as f:
+                file_content = f.read()  # comes out as bytes
+            try:
+                file_content = file_content.decode("utf-8") # convert to a string
+            except AttributeError:
+                pass # wasn't bytes, ignore
+            content = file_content.split('\n')
+
+            return content
+
+        else:
+            with open(os.path.join(log_dir, log_file), 'r') as f:
+                content = f.read()
+
+            return content
+
+
+    def parse_sshd_content(self, content):
+
+        matches = {}
+
+        for line in content:
+            # Catch the usual
+            # Jun 10 12:40:05 defestri sshd[11019]: Invalid user admin from 61.174.51.217
+            m = re.search(SEARCH_STRING, line)
+            if m is None:
+                # Also catch these
+                # Jun  8 04:31:10 defestri sshd[5013]: User root from 116.10.191.234 not allowed because none of user's groups are listed in AllowGroups
+                m = re.search(ROOT_NOT_ALLOWED_SEARCH_STRING, line)
+            if m:
+                if m.group('log_date')[:3] == last_month.strftime('%b'):
+                    log_date = datetime.strptime(m.group('log_date'), '%b %d %H:%M:%S')
+                    log_date = log_date.replace(year=last_month.year)
+                    # Set the time zone info for the system
+                    #log_date = log_date.replace(tzinfo=sys_tz)
+                    # Convert it to UTC time
+                    #log_date = log_date.astimezone(pytz.utc)
+
+                    #sometimes there's multiple entries per second, since we're
+                    #not that concerned about to the second accuracy just increment
+                    #the seconds until we find a unique log date to put in
+                    if log_date in matches:
+                        while log_date in matches:
+                            ns = log_date.second+1
+                            if ns >= 60:
+                                ns = 0
+                            log_date = log_date.replace(second=ns)
+                    matches[log_date] = (m.group('ip_add'), m.group('user'))
+
+        return matches
+
+
+    def read_sshd_logs(self):
+
+        matches = {}
+
+        for log_file is os.listdir(self.LOG_DIR):
+            if 'auth.log' in log_file or 'secure' in log_file:
+                modified_time = datetime.strptime(time.ctime(os.path.getmtime(
+                                os.path.join(log_dir, log_file))), "%a %b %d %H:%M:%S %Y")
+                if modified_time > self.last_run():
+                    content = self.get_file_content(log_file)
+                    file_matches = parse_sshd_content(content)
+
 
 
     def read_logs(self, log_dir):
